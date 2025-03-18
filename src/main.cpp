e075831773f97609fcf10dbdafcabaac8350bbdd
DESKTOP-6NRT369\Dell <minh.trannhat2911@hcmut.edu.vn>
2025-03-18 16:04:41 +0700

    Code

---
+++ src/main.cpp
@@ -0,0 +1,116 @@
+#define SDA_PIN GPIO_NUM_11
+#define SCL_PIN GPIO_NUM_12
+
+#include <WiFi.h>
+#include <Arduino_MQTT_Client.h>
+#include <ThingsBoard.h>
+#include "DHT20.h"
+#include "Wire.h"
+#include <ArduinoOTA.h>
+
+constexpr char WIFI_SSID[] = "Min";
+constexpr char WIFI_PASSWORD[] = "123456789";
+
+constexpr char TOKEN[] = "mae15of5vf8oc2v3bdap";
+
+constexpr char THINGSBOARD_SERVER[] = "app.coreiot.io";
+constexpr uint16_t THINGSBOARD_PORT = 1883U;
+
+constexpr uint32_t MAX_MESSAGE_SIZE = 1024U;
+constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
+
+
+uint32_t previousStateChange;
+
+constexpr int16_t telemetrySendInterval = 10000U;
+uint32_t previousDataSend;
+
+WiFiClient wifiClient;
+Arduino_MQTT_Client mqttClient(wifiClient);
+ThingsBoard tb(mqttClient, MAX_MESSAGE_SIZE);
+
+DHT20 dht20;
+
+float temperature = NAN;
+float humidity = NAN;
+
+void InitWiFi() {
+  Serial.println("Connecting to AP ...");
+  // Attempting to establish a connection to the given WiFi network
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    // Delay 500ms until a connection has been successfully established
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to AP");
+}
+
+const bool reconnect() {
+  // Check to ensure we aren't connected yet
+  const wl_status_t status = WiFi.status();
+  if (status == WL_CONNECTED) {
+    return true;
+  }
+  // If we aren't establish a new connection to the given WiFi network
+  InitWiFi();
+  return true;
+}
+
+void setup() {
+  Serial.begin(SERIAL_DEBUG_BAUD);
+  delay(1000);
+  Serial.println("Initializing WiFi...");
+  InitWiFi();
+
+  Wire.begin(SDA_PIN, SCL_PIN);
+  dht20.begin();
+}
+
+void loop() {
+    delay(10);
+  
+    if (!reconnect()) {
+      return;
+    }
+
+    if (!tb.connected()) {
+      Serial.print("Connecting to: ");
+      Serial.print(THINGSBOARD_SERVER);
+      Serial.print(" with token ");
+      Serial.println(TOKEN);
+      if (!tb.connect(THINGSBOARD_SERVER, TOKEN, THINGSBOARD_PORT)) {
+        Serial.println("Failed to connect");
+        return;
+      }
+    }  
+    if (millis() - previousDataSend > telemetrySendInterval) {
+      previousDataSend = millis();
+
+      dht20.read();
+      
+      float temperature = dht20.getTemperature();
+      float humidity = dht20.getHumidity();
+
+      if (isnan(temperature) || isnan(humidity)) {
+        Serial.println("Failed to read from DHT20 sensor!");
+      } else {
+        Serial.print("Temperature: ");
+        Serial.print(temperature);
+        Serial.print(" °C, Humidity: ");
+        Serial.print(humidity);
+        Serial.println(" %");
+
+        tb.sendTelemetryData("temperature", temperature);
+        tb.sendTelemetryData("humidity", humidity);
+      }
+
+      tb.sendAttributeData("rssi", WiFi.RSSI());
+      tb.sendAttributeData("channel", WiFi.channel());
+      tb.sendAttributeData("bssid", WiFi.BSSIDstr().c_str());
+      tb.sendAttributeData("localIp", WiFi.localIP().toString().c_str());
+      tb.sendAttributeData("ssid", WiFi.SSID().c_str());
+    }
+
+    tb.loop();
+  }
